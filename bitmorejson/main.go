d18adadd4fb6ff99857cbd71ab7f3178fb1138e2
Zeeshan Ahmad <zeeshanahmad@dtek.ai>
2024-05-01 18:43:51 +0500

    learned how to decode jso

--- bitmorejson/main.go
+++ bitmorejson/main.go
@@ -15,8 +15,8 @@ type course struct {
 func main() {
 	fmt.Println("Welcome to a bitmorejso")
 
-	EncodeJson()
-
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -35,5 +35,33 @@ func EncodeJson() {
 func checkErr(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func DecodeJson() {
+	jsonFromWeb := []byte(`
+	{
+		"title": "flutter development",
+		"price": 199,
+		"tags": [
+				"flutter",
+				"dev"
+		]
+	}`)
+	if json.Valid(jsonFromWeb) {
+		fmt.Println("json is valid")
+		var lcoCourse course
+		json.Unmarshal(jsonFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+		fmt.Printf("%s\n", lcoCourse.Tags)
+	} else {
+		fmt.Println("JSON IS NOT VALID")
+	}
+
+	var onlineData map[string]interface{}
+	json.Unmarshal(jsonFromWeb, &onlineData)
+
+	for k, v := range onlineData {
+		fmt.Printf("Key is %v and Value is %v\n", k, v)
 	}
 }
